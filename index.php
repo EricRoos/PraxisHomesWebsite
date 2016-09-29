5301c44ba67673095d636e83e96c4ee2265efacd
Eric Roos <ericroos13@gmail.com>
2016-09-29 18:25:22 -0400

    refactor topbar to be partial

--- index.php
+++ index.php
@@ -13,32 +13,8 @@
 
 
 	<body>
-		<div class='praxis-top-bar'>
-			<div class='left'>
-				<h1>Praxis Homes</h1>
-			</div>
-			<div class='right show-for-small-only mobile-nav-right'>
-				<ul class="dropdown menu" data-dropdown-menu>
-					<li>
-						<a href="#"><i class='fa fa-bars fa-fw'></i></a>
-						<ul class="menu">
-							<li><a class='dropdown-link' href='/blog'>blog</a></li>
-							<li><a class='dropdown-link' href='/portfolio'>portfolio</a></li>
-<li><a class="dropdown-link typeform-share link" href="https://eric422.typeform.com/to/xLUuoI" data-mode="1" target="_blank">contact me</a></li>
-<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
-						</ul>
-					</li>
-				</ul>
-			</div>
-			<div class='right hide-for-small-only'>
-				<a href='/blog'>blog</a>
-				<a href='/portfolio'>portfolio</a>
-<a class="typeform-share link" href="https://eric422.typeform.com/to/xLUuoI" data-mode="1" target="_blank">contact me</a>
-<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
+		<?php include('partials/_topbar.php'); ?>
 
-			</div>
-
-		</div>
 		<div class="parallax-background">
 			<div class='front-image'>
 				<div class="intro-text">
