1c3f29cce266b7637b82fef0b13ac76883d2f855
Eric Roos <ericroos13@gmail.com>
2016-10-11 23:10:28 -0400

    done

--- partials/_topbar.php
+++ partials/_topbar.php
@@ -7,7 +7,6 @@
 			<li>
 				<a href="#"><i class='fa fa-bars fa-fw'></i></a>
 				<ul class="menu">
-					<li><a class='dropdown-link' href='/blog'>blog</a></li>
 					<li><a class='dropdown-link' href='/portfolio'>portfolio</a></li>
 <li><a class="dropdown-link typeform-share link" href="https://eric422.typeform.com/to/xLUuoI" data-mode="1" target="_blank">contact me</a></li>
 <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
@@ -16,7 +15,6 @@
 		</ul>
 	</div>
 	<div class='right hide-for-small-only'>
-		<a href='/blog'>blog</a>
 		<a href='/portfolio'>portfolio</a>
 <a class="typeform-share link" href="https://eric422.typeform.com/to/xLUuoI" data-mode="1" target="_blank">contact me</a>
 <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
